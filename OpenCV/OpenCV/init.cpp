6ec0a9923039489fbe24b8cac32ab8f1a2da806c
Starpia <starpia@mail.com>
2018-05-27 17:08:37 +0900

    complete of analyze video class

--- OpenCV/OpenCV/init.cpp
+++ OpenCV/OpenCV/init.cpp
@@ -46,27 +46,25 @@ public:
 		orgVideo.release();
 	}
 
-
-	void TopHatView()
+	void analyzeVideo()
 	{
-		Mat kernel(5, 5, CV_8U);
+		
 		Mat frame;
 
 		while (true)
 		{
 			orgVideo >> frame;
+			Mat analyzeFrame;
 
 			if (frame.empty())
 			{
 				break;
 			}
 
-			cvtColor(frame, frame, COLOR_BGR2GRAY);
-			Canny(frame, frame, 150, 210);
-			//equalizeHist(frame, frame);
-			//cvtColor(frame, frame, COLOR_GRAY2BGR);
-			//morphologyEx(frame, frame, MORPH_TOPHAT, kernel);
-			imshow("Morphology Video", frame);
+			cvtColor(frame, analyzeFrame, COLOR_BGR2GRAY);
+			halfOfImage(analyzeFrame);
+			analyzeImage(analyzeFrame);
+			showOverayImage(frame, analyzeFrame);
 
 			char c = (char)waitKey(1);
 			if (c == 27)
@@ -78,6 +76,53 @@ public:
 		return;
 	}
 
+	void showOverayImage(Mat &_oriImg, Mat &_analyzeImg)
+	{
+		static int horizon = (_oriImg.rows / 2) - 30;
+
+		for (int i = horizon; i < _oriImg.rows; i++)
+		{
+			for (int j = 0; j < _oriImg.cols; j++)
+			{
+				if (_analyzeImg.at<uchar>(i, j) > 0)
+				{
+					_oriImg.at<Vec3b>(i, j)[0] = 0;
+					_oriImg.at<Vec3b>(i, j)[1] = 0;
+					_oriImg.at<Vec3b>(i, j)[2] = 255;
+				}
+			}
+		}
+
+		imshow("Analyzed Image", _oriImg);
+	}
+
+	void analyzeImage(Mat &_img)
+	{
+		Mat kernel(5, 5, CV_8U);
+		Mat kernel2(13, 13, CV_8U);
+		//cvtColor(_img, _img, COLOR_BGR2GRAY);
+		morphologyEx(_img, _img, MORPH_TOPHAT, kernel);
+		Canny(_img, _img, 200, 230);
+		//dilate(_img, _img, kernel);
+		//medianBlur(_img, _img, 11);
+		morphologyEx(_img, _img, MORPH_CLOSE, kernel);
+		medianBlur(_img, _img, 13);
+		dilate(_img, _img, kernel2);
+		Canny(_img, _img, 1, 200);
+	}
+
+	void halfOfImage(Mat &_img)
+	{
+		static int horizon = (_img.rows / 2) - 20;
+
+		for (int i = 0; i < (horizon); i++)
+		{
+			for (int j = 0; j < _img.cols; j++)
+			{
+				_img.at<uchar>(i, j) = 0;
+			}
+		}
+	}
 
 };
 
@@ -86,7 +131,7 @@ int main(int argc, char **argv)
 
 	VideoAnalyzer va(argv[1]);
 
-	va.TopHatView();
+	va.analyzeVideo();
 
 	return 0;
 
